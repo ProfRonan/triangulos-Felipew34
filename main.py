266872bb9702e7c3dac17c7bb6602bcd8f858941
Felipew34 <felipew34@alu.ufc.br>
2020-12-21 14:47:20 +0000

    Adicionei exercício

--- main.py
+++ main.py
@@ -4,6 +4,25 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-
+    a = float(input("Digite o primeiro lado\n"))
+    b = float(input("Digite o segundo lado\n"))
+    c = float(input("Digite o terceiro lado\n"))
+    tipo = str("null")
+    if (a + b <= c) or (a + c <= b) or (b + c <= a):
+      validade = 0
+    else: 
+      validade = 1
+    
+    if validade == 0:
+      print("Os valores informados não formam um triângulo.")
+    else:
+      if a==b and b==c:
+        tipo = str("equilátero")
+      elif a==b or b==c or a==c:
+        tipo = str("isóceles")
+      else: 
+        tipo = str("escaleno")
+      
+    print(f'O triângulo é {tipo}.')
 if __name__ == '__main__':
     main()
